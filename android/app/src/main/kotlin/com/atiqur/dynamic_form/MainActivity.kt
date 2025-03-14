07e50ee393e8b5ab9012317e47b615b0c20f1233
Atiqur Rahman <atiqrs.me@gmail.com>
2025-03-14 08:44:26 +0600

    setup core files

---
+++ android/app/src/main/kotlin/com/atiqur/dynamic_form/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.atiqur.dynamic_form
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants {
+  static const String baseUrl = "http://team.dev.helpabode.com:54292";
+  static const String formDataEndpoint = "/api/wempro/flutter-dev/coding-test-2025/";
+}
